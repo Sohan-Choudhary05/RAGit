cb78697d73b0d38bd333561c5c36d41e51be3e8d
Sohan-Choudhary05 <choudharysohan053@gmail.com>
2025-07-08 19:02:12 +0530

    app created

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
+from sqlalchemy import Column,String,Integer,Text
+
+app = Flask(__name__)
+app.config.from_object('config.Config')
+db = SQLAlchemy(app)
+jwt = JWTManager(app)
+migrate = Migrate(app,db)
+
+@app.cli.command('db_create')
+def db_create():
+    db.create_all()
+    print("Database Created!")
+
+@app.cli.command("db_drop")
+def db_drop():
+    db.drop_all()
+    print("Database dropped successfully")
+
+
+import routes
